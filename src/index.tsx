d56cb891ccad98c2d327dc914b1c6e30e2a58eb2
Lebchen <lebchen@gmail.com>
2021-09-05 14:03:53 +0200

    Hello world.

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Skur from "./Skur";
+
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Skur></Skur>
+	</React.StrictMode>,
+	document.getElementById('skur')
+);
